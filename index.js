0af4cc8b8d70515dbce3293585c6b1f7f781424d
rahhalali <alih.rahhal@hotmail.com>
2020-08-02 22:54:02 +0300

    hello

---
+++ index.js
@@ -0,0 +1,118 @@
+// // function div(a ,b){
+// //      return a/b;
+// //  }
+// // div(7,3);
+// //  var a =function(a,b){
+// // return a/b;
+// // }
+// //  console.log(a(5,3));
+
+
+// // function checkDriverAge(){
+
+// // var age =prompt("what is your age?");
+
+// // if (Number(age) < 18) {
+// // 	alert("Sorry, you are too yound to drive this car. Powering off");
+// // } else if (Number(age) > 18) {
+// // 	alert("Powering On. Enjoy the ride!");
+// // } else if (Number(age) === 18) {
+// // 	alert("Congratulations on your first year of driving. Enjoy the ride!");
+// // }
+
+// // }
+// // checkDriverAge();
+
+// // function checkDriverAge(a){
+
+// //     console.log("what is your age?");
+    
+// //     if (a < 18) {
+// //         console.log("Sorry, you are too yound to drive this car. Powering off");
+// //     } else if (a > 18) {
+// //         console.log("Powering On. Enjoy the ride!");
+// //     } else if (a === 18) {
+// //         console.log("Congratulations on your first year of driving. Enjoy the ride!");
+// //     }else if(a === 92){
+// //        console.log ("Power on.enjoy the ride");
+// //     }
+    
+// //     }
+// //     console.log(checkDriverAge(92));
+
+// // var user ={
+// // name :"john",
+// // age:50,
+// // hoppy:"soccer ",
+// // list :["ali","abbas","mohammad"]
+
+// // }
+// //  var list = [
+// // {
+// // username : "ali",
+// // password: "123"
+
+// // },
+// // {
+// // username :"abbas",
+// // password :"4567"
+
+// // }
+
+
+// //  ];
+
+// // var users = [
+// //     {
+// // username : "ali",
+// // password: "prof"
+// //     },{
+// // username : "mohamad",
+// // password: "prof"
+// //     }
+// // ];
+// // var title= [
+// // {
+// // username :"abbas",
+// // title :"hey h r u"
+// // },
+// // {
+// //     username :"hussien",
+// //     title :"habebe ya shrek"
+// // }
+
+// // ];
+// // function isvalid(username ,password){
+// // for(var i=0;i<users.length;i++){
+// //     if((username === users[i].username && password === users[i].password )){
+// //        return true;
+// //     }
+// // }
+// // return false;
+// // }
+// // function sing(user,pass){
+// //     if(isvalid(user,pass)){
+// //         console.log(title);
+// //     }else{
+// //         alert("soory you cant access");
+// //     }
+// // }
+// // var usern =prompt("what is your name ?");
+// // var passw =prompt("what is your password");
+// // sing(usern,passw);
+
+// // exercise 3 
+
+// // var firstnumber = prompt("enter first number");
+// // var secondnumber = prompt("enter first number");
+
+// // alert(Number(firstnumber)+ Number(secondnumber));
+ alert(Number(firstnumber) - Number(secondnumber));
+alert(Number(firstnumber) * Number(secondnumber));
+
+var array = ["apple", "Apples", "Oranges", "Blueberries"];
+
+
+
+
+    
